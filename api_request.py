f559ded127dccaf2f3894812fe634dbe1c54e8c4
Mohammad Albuainain <moh_dev@icloud.com>
2025-01-22 00:40:33 +0300

    Added Error handling

--- api_request.py
+++ api_request.py
@@ -2,6 +2,7 @@ import os
 
 import requests
 from dotenv import load_dotenv
+from flask import flash
 
 load_dotenv()
 
@@ -20,27 +21,35 @@ class WeatherApi:
         self.feels_like = None
 
     def get_weather(self):
-        response = requests.get(
-            f"https://api.weatherstack.com/current?access_key={
-                self.access_key}&query={self.query}"
-        )
-        data = response.json()
-        self.city = data.get("request", {}).get("query", "Unknown Location")
-        self.weather_descriptions = data.get("current", {}).get(
-            "weather_descriptions", "N/A"
-        )[0]
-        self.is_day = data.get("current", {}).get("is_day", "N/A")
-        self.weather_icons = data.get("current", {}).get(
-            "weather_icons", ["N/A"])[0]
-        self.feels_like = f"{
-            data.get('current', {}).get('feelslike', 'N/A')} C"
-        self.temperature = f"{
-            data.get('current', {}).get('temperature', "N/A")} C"
-        self.wind_speed = f"{
-            data.get('current', {}).get('wind_speed', "N/A")} km/h"
-        self.humidity = f"{data.get('current', {}).get('humidity', "N/A")} %"
+        try:
+            response = requests.get(
+                f"https://api.weatherstack.com/current?access_key={self.access_key}&query={self.query}")
+            data = response.json()
+            if "error" in data:
+                print(f"API Error : {data['error']['info']}")
+                return False
+            self.city = data.get("request", {}).get(
+                "query", "Unknown Location")
+            self.weather_descriptions = data.get(
+                "current", {}).get("weather_descriptions", "N/A")[0]
+            self.is_day = data.get("current", {}).get("is_day", "N/A")
+            self.weather_icons = data.get("current", {}).get(
+                "weather_icons", ["N/A"])[0]
+            self.feels_like = f"{
+                data.get('current', {}).get('feelslike', 'N/A')} C"
+            self.temperature = f"{
+                data.get('current', {}).get('temperature', 'N/A')} C"
+            self.wind_speed = f"{
+                data.get('current', {}).get('wind_speed', 'N/A')} km/h"
+            self.humidity = f"{
+                data.get('current', {}).get('humidity', 'N/A')} %"
+            return True
+        except requests.exceptions.RequestException as e:
+            flash("Request error", "danger")
+            print(f"Request error {e}")
+            return False
 
 
 if __name__ == "__main__":
-    app = WeatherApi()
+    app = WeatherApi("Riyadh")
     app.get_weather()
--- app/routes.py
+++ app/routes.py
@@ -9,9 +9,15 @@ main = Blueprint('main', __name__)
 @main.route('/', methods=['GET', 'POST'])
 def index():
     city = None
+    weather_status = None
     if request.method == "POST":
         city = request.form.get("city")
-        weather_status = WeatherApi(city)
-        weather_status.get_weather()
+        if city:
+            weather_status = WeatherApi(city)
+            if not weather_status.get_weather():
+                flash("Unable to fetch weather data. Please try again.", "danger")
+                weather_status = None
+    else:
+        weather_status = None
 
     return render_template('home.html', city=city, weather_status=weather_status)
